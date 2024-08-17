62e5fee1f8fc9fc8a618b645b8ef0d8ecfa6a2fa
Berlin Brown <berlin.brown@gmail.com>
2024-08-17 18:10:38 -0400

    Update BotListUserLinkDAO.java

--- botlist/src/org/spirit/dao/BotListUserLinkDAO.java
+++ botlist/src/org/spirit/dao/BotListUserLinkDAO.java
@@ -6,9 +6,6 @@ package org.spirit.dao;
 
 import org.spirit.bean.impl.BotListUserLink;
 
-/**
- * @author AP417
- */
 public interface BotListUserLinkDAO {
 	
 	public abstract void createLink(BotListUserLink link);		
